3362880f674bb032381cce48b34517f9c4408d09
708vipin <pvipin708@gmail.com>
2025-08-20 09:56:10 -0700

    Comparisons

---
+++ 01_core_basics/main.py/15. If_statements_&_comparisons.py
@@ -0,0 +1,13 @@
+def max_num(num1, num2, num3):
+    if num1 >= num2 and num1 >= num3:
+        return num1
+    elif num2 >= num1 and num2 >= num3:
+        return num2
+    else:
+        return num3
+    
+print(max_num(48, 39, 1118))    
+
+#Different Types of Operator
+# Not equal !=
+# Equal ==
