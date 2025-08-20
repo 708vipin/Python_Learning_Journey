403a0331805a5365596b2b4cd96e35d460aa3976
708vipin <pvipin708@gmail.com>
2025-08-20 01:48:20 -0700

    Tuples

---
+++ 01_core_basics/main.py/11. Tuples.py
@@ -0,0 +1,8 @@
+coordinates = (4,5)        #Creating a tuple
+                           #Tuples are immutable - means you can't  modify once created
+print(coordinates[0])
+
+#Difference between Tuples and Lists
+#1. Tuples are immutable, Lists are mutable.
+
+friends_location_list = [(3,4), (5,9), (8.3, 6.7)]        #List of Tuples.
