40a96daab00cea024d6e7641a1d59c2c5fd2aa7b
pvipin708 <pvipin708@gmail.com>
2025-08-21 18:24:56 +0530

    Dictionaries

---
+++ 01_core_basics/main.py/17. Dictionaries.py
@@ -0,0 +1,12 @@
+month_converter = {
+    "Jan" : "January",
+    "Feb" : "February",
+    "Mar" : "March",
+    1729 : "I'm learning the concept of Dictionaries in Python"
+}
+
+#Dictionaries are created with {}
+#Keys can be strings as well as number, just be UNIQUE
+
+print(month_converter.get("Jan"))
+print(month_converter.get(1729))
